397832ad108ac51dc210161d757ca25e5b4bcb3b
lisha-cashify <lisha.1@cashify.in>
2025-05-14 11:45:11 +0530

    hii

---
+++ screens/MealsOverview.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useLayoutEffect} from 'react';
+import {MEALS, CATEGORIES} from '../data/dummy-data'
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import MealItem from '../components/MealItem.tsx';
+
+function MealsOverviewScreen({ route, navigation }) {
+    const catId = route.params.categoryId;
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find(
+            (category) => category.id === catId
+        ).title;
+
+        navigation.setOptions({
+            title: categoryTitle,
+        });
+    }, [catId, navigation]);
+
+
+    function renderMealItem(itemData) {
+        const mealItemProps ={
+            title: itemData.item.title,
+            imageUrl: itemData.item.imageUrl,
+            duration: itemData.item.duration,
+            complexity: itemData.item.complexity,
+            affordability: itemData.item.affordability
+        }
+        return <MealItem {...mealItemProps} />;
+
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
+        </View>
+    );
+}
+
+
+export default MealsOverviewScreen;
+const styles = StyleSheet.create({
+    container :{
+        flex:1,
+        padding:16,
+
+    }
+})
+
